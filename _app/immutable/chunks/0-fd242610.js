7ad15fd9d85ee6e8d693cb9201415081a84b5e43
burningtree <burningtree@users.noreply.github.com>
2023-04-25 08:23:45 +0000

    deploy: 01723c3544f1dc9d4ee755d28406870778f9f5be

--- _app/immutable/chunks/0-fd242610.js
+++ _app/immutable/chunks/0-fd242610.js
@@ -1,5 +1,5 @@
 import { _ } from "./_layout-308acc63.js";
-import { default as default2 } from "../components/pages/_layout.svelte-a349a0b2.js";
+import { default as default2 } from "../components/pages/_layout.svelte-1050aff9.js";
 export {
   default2 as component,
   _ as universal
