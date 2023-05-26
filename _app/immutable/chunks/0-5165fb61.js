1cb97082b2d2e048a89b8f1e93358f6232856615
Msiusko <Msiusko@users.noreply.github.com>
2023-05-26 12:17:05 +0000

    deploy: 26f3b2607eac1d9b0d68ec19dacdce9dc1e9617f

--- _app/immutable/chunks/0-5165fb61.js
+++ _app/immutable/chunks/0-5165fb61.js
@@ -1,4 +1,4 @@
-import { _ } from "./_layout-f7c9233d.js";
+import { _ } from "./_layout-f9bbae3c.js";
 import { default as default2 } from "../components/pages/_layout.svelte-c74b8e82.js";
 export {
   default2 as component,
--- _app/immutable/modules/pages/_layout.js-3775daa4.js
+++
@@ -1,4 +0,0 @@
-import { l } from "../../chunks/_layout-f7c9233d.js";
-export {
-  l as load
-};
